a79403bce222f0f6e77a12f77419f7a4dced74ca
Sanjay Vasuniya <sanjayv072004@gmail.com>
2025-12-10 10:59:19 +0530

    done

--- NodeJs-fileReading-writing/index.js
+++ NodeJs-fileReading-writing/index.js
@@ -1,4 +1,4 @@
-// firstly requiring the fs (file system)
+/*// firstly requiring the fs (file system)
 const fs = require("fs");
 
 // readfilesync used for read the file synchronous way
@@ -6,4 +6,15 @@ const textIn = fs.readFileSync("input.txt", "utf-8");
 
 // writefilesync used for write the file
 fs.writeFileSync("output.txt", `written file is :- ${textIn}`);
-console.log("file written !");
+console.log("file written !");*/
+
+/// now its turn to write code in asynchronus way
+const fs = require("fs");
+fs.readFile("input.txt", "utf-8", (err, data1) => {
+  fs.readFile("output.txt", "utf-8", (err, data2) => {
+    fs.writeFile("append.txt", `${data1}\n ${data2}`, "utf-8", (err) => {
+      console.log("file is written !");
+    });
+  });
+});
+console.log("code running......");
